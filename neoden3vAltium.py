c0f20c2083860a6be11f84fb67646e2217c321f5
Georgii Evdokimov <gmevdokimov@yandex-team.ru>
2024-04-14 02:53:40 +0300

    new features: -mix

--- neoden3vAltium.py
+++ neoden3vAltium.py
@@ -5,23 +5,47 @@
 import sys
 from decimal import Decimal
 import argparse
+import re
+
+
+class Columns:
+    def __init__(self):
+        self.designator_position = 0
+        self.comment_position = 1
+        self.layer_position = 2
+        self.footprint_position = 3
+        self.x_position = 4
+        self.y_position = 5
+        self.rotation_position = 6
+    def print(self):
+        print("Des %i Com %i L %i fp %i X %i Y %i Rot %i" %
+              (self.designator_position, self.comment_position, self.layer_position,
+               self.footprint_position, self.x_position, self.y_position, self.rotation_position))
 
 
 class Component:
     # just a structure to represent a physical component
-    def __init__(self, line):
+    def __init__(self, line, columns):
         # "Designator","Comment","Layer","Footprint","Center-X(mm)","Center-Y(mm)","Rotation","Description"
-        self.Designator = line.split(',')[0]
-        self.Footprint = (str(line.split(',')[3]).replace("\"", ""))
-        self.Layer = (str(line.split(',')[2]).replace("\"", ""))
-        self.X = line.split(',')[4].replace("\"", "")
-        self.Y = line.split(',')[5].replace("\"", "")
-        self.Layer = line.split(',')[2].replace("\"", "")
-        self.Rotation = line.split(',')[6].replace("\"", "")
-        self.Comment = line.split(',')[1].replace("\"", "")
+        self.Designator = line.split(',')[columns.designator_position]
+        self.Footprint = (str(line.split(',')[columns.footprint_position]).replace("\"", ""))
+        self.Layer = (str(line.split(',')[columns.layer_position]).replace("\"", ""))
+        self.X = line.split(',')[columns.x_position].replace("\"", "")
+        self.Y = line.split(',')[columns.y_position].replace("\"", "")
+        #self.Layer = line.split(',')[2].replace("\"", "")
+        if self.Layer == "top":
+            self.Layer = "TopLayer"
+        else:
+            if self.Layer == "bottom" or self.Layer == "bot":
+                self.Layer = "BottomLayer"
+        self.Rotation = line.split(',')[columns.rotation_position].replace("\"", "")
+        self.RawRotation = self.Rotation
+        self.Comment = line.split(',')[columns.comment_position].replace("\"", "")
         self.nozzle = 0
         self.feeder = 0
         self.skip = 'No'
+        self.Correction = "0"
+
 
 
 class CorrectionForElement:
@@ -43,12 +67,43 @@ class Part:
 class NeoDenConverter:
     def make_component_list(self):
         counter = 0
+        preamble = True
+
+        designator_regexp = "(Designator)|(Ref)"
+        comment_regexp = "(Comment)|((^ *Val,)|(, *Val))"
+        layer_regexp = "([,\"']{0,}[^a-zA-Z]Layer)|(Side)"
+        footprint_regexp = "(Footprint)|(Package)"
+        x_regexp = "(Center-X)|(PosX)"
+        y_regexp = "(Center-Y)|(PosY)"
+        rotation_regexp = "(Rotation)|(Rot)"
+
         for line in self.AltiumOutputFile:
-            if counter < 13:
+            """if counter < 13:
                 # throw away the header.
-                pass
+                pass"""
+            if preamble:
+                x_position = re.search(x_regexp, line, re.MULTILINE)
+                y_position = re.search(y_regexp, line, re.MULTILINE)
+                rotation = re.search(rotation_regexp, line, re.MULTILINE)
+                if x_position is not None and y_position is not None and rotation is not None:
+                    designator = re.search(designator_regexp, line, re.MULTILINE)
+                    comment = re.search(comment_regexp, line, re.MULTILINE)
+                    layer = re.search(layer_regexp, line, re.MULTILINE)
+                    footprint = re.search(footprint_regexp, line, re.MULTILINE)
+
+                    self.columns.x_position = line[0:x_position.span()[0] + 1].count(",")
+                    self.columns.y_position = line[0:y_position.span()[0] + 1].count(",")
+                    self.columns.rotation_position = line[0:rotation.span()[0] + 1].count(",")
+                    self.columns.designator_position = line[0:designator.span()[0] + 1].count(",")
+                    self.columns.comment_position = line[0:comment.span()[0] + 1].count(",")
+                    self.columns.layer_position = line[0:layer.span()[1]].count(",")
+                    print(line[0:layer.span()[0] + 2])
+                    self.columns.footprint_position = line[0:footprint.span()[0] + 1].count(",")
+                    print("COLUMNS:")
+                    self.columns.print()
+                    preamble = False
             else:
-                new_element = Component(line)
+                new_element = Component(line, self.columns)
                 self.components.append(new_element)
                 self.footprints.add(new_element.Footprint)
 
@@ -96,7 +151,7 @@ class NeoDenConverter:
                            str(comp.Designator).replace("\"", "") + "," + \
                            comp.Comment + "," + str(comp.Footprint).replace("\"", "") + "," + \
                            str(round(Decimal(comp.X), 2)) + "," + str(round(Decimal(comp.Y), 2)) + "," + \
-                           str(comp.Rotation).replace("\"", "") + "," + comp.skip + ","
+                           (str(comp.Rotation).replace("\"", "")).replace("\n", "") + "," + comp.skip + ","
                 output_file.write(out_line + "\n")
 
     def create_footprints_file(self):
@@ -146,6 +201,7 @@ class NeoDenConverter:
             for correction in self.corrections:
                 if component.Footprint == correction.Footprint and correction.Correction != 0:
                     component.Rotation = str(float(correction.Correction) + float(component.Rotation))
+                    component.Correction = correction.Correction
                     corrected.add(component.Footprint)
                     break
         print("angles corrected for: ")
@@ -169,6 +225,8 @@ class NeoDenConverter:
         for component in self.components:
             part_str = component.Footprint + '\\' + component.Comment
             component.skip = 'Yes'
+            component.feeder = self.default_feeder
+            component.nozzle = self.default_nozzle
             for conf in feeders_confs:
                 if part_str == conf.part:
                     if conf.feeder and conf.nozzle:
@@ -176,8 +234,7 @@ class NeoDenConverter:
                         component.nozzle = conf.nozzle
                         component.skip = 'No'
                     else:
-                        component.feeder = self.default_feeder
-                        component.nozzle = self.default_nozzle
+                        pass
         return 0
 
     def move_angels_to_m180to180(self):
@@ -189,8 +246,45 @@ class NeoDenConverter:
 
     def flip_board(self):
         for comp in self.components:
-            comp.X = str(float(comp.X) * (-1))
-            comp.Rotation = str(360.00 - float(comp.Correction))
+            try:
+                comp.X = str(float(comp.X) * (-1))
+                comp.Rotation = str(360.00 - float(comp.Rotation))  #ЧЁ ТУТ БЫЛО??????
+            except ValueError:
+                print("===---*-*-*-*-*-*-=======")
+                print(comp.Footprint)
+                print("==========---*-*-*-*-*-*-")
+
+    def mix_nozzles(self):
+        nozzles_one_or_less = list()
+        nozzles_other = list()
+        nozzles_other_index = 0
+        skiped_components = list()
+        mixed_components = list()
+
+        for comp in self.components:
+            if comp.skip == 'Yes':
+                skiped_components.append(comp)
+            else:
+                if comp.nozzle <= 1 :
+                    nozzles_one_or_less.append(comp)
+                else:
+                    nozzles_other.append(comp)
+        for comp_nozzle_1 in nozzles_one_or_less:
+            mixed_components.append(comp_nozzle_1)
+            if nozzles_other_index < len(nozzles_other):
+                mixed_components.append(nozzles_other[nozzles_other_index])
+                nozzles_other_index += 1
+        print("Mixed %i lines\n"%(nozzles_other_index))
+        while nozzles_other_index < len(nozzles_other):
+            mixed_components.append(nozzles_other[nozzles_other_index])
+            nozzles_other_index += 1
+        for comp in skiped_components:
+            mixed_components.append(comp)
+        self.components = mixed_components
+        return
+
+
+
 
     def __init__(self, file_name):
         try:
@@ -198,6 +292,7 @@ class NeoDenConverter:
         except FileNotFoundError:
             print("No such file\n")
             exit(-1)
+        self.columns = Columns()
         self.footprints = set()
         self.components = list()
         self.parts_names = set()
@@ -210,8 +305,8 @@ class NeoDenConverter:
         self.flip_flag = False
         self.firstChipPhysicalX = 0.0
         self.firstChipPhysicalY = 0.0
+        self.columns = Columns()
         return
-
 
 description = "Program for creating .CSV files for Neoden3V"
 file_help = '*.csv file from Altium'
@@ -222,7 +317,8 @@ feed_help = "include components and feeders pairs from file PC"
 top_help = "filter only top layer components"
 bot_help = "filter only bot layer components"
 flip_help = "flip board"
-
+mix_help = "Mix component list between nozzles"
+print_help = "Print result while generating"
 
 def create_parser():
     arg_parser = argparse.ArgumentParser(description=description)
@@ -232,6 +328,8 @@ def create_parser():
     arg_parser.add_argument('-top', action=argparse.BooleanOptionalAction, type=bool, default=False, help=top_help)
     arg_parser.add_argument('-bot', action=argparse.BooleanOptionalAction, type=bool, default=False, help=bot_help)
     arg_parser.add_argument('-flip', action=argparse.BooleanOptionalAction, type=bool, default=False, help=flip_help)
+    arg_parser.add_argument('-mix', action=argparse.BooleanOptionalAction, type=bool, default=False, help=mix_help)
+    arg_parser.add_argument('-print', action=argparse.BooleanOptionalAction, type=bool, default=False, help=print_help)
     arg_parser.add_argument('-cf', type=str, help=cf_help)
     arg_parser.add_argument('-feed', type=str, help=feed_help)
     return arg_parser
@@ -256,6 +354,13 @@ if args.cl:
     converter.create_parts_file()
     no_output_generate_flag = True
 
+if no_output_generate_flag:
+    exit(0)
+
+if args.flip:
+    converter.flip_board()
+
+
 if args.cf:
     if 0 == converter.make_angles_correction(args.cf):
         print("successful correction\n")
@@ -268,9 +373,6 @@ if args.feed:
     else:
         exit(0)
 
-if no_output_generate_flag:
-    exit(0)
-
 if args.top:
     layer_flag = 'TopLayer'
 else:
@@ -279,15 +381,25 @@ else:
     else:
         layer_flag = None
 
-if args.flip:
-    converter.flip_board()
-
-
 converter.move_angels_to_m180to180()
+
 converter.get_distances_from_first_chip()
 converter.firstChipPhysicalX = float(
     input("Enter the machine X coordinate of component " + converter.components[0].Designator + " : "))
 converter.firstChipPhysicalY = float(
     input("Enter the machine Y coordinate of component " + converter.components[0].Designator + " : "))
 converter.apply_machine_positions_2_components()
+
+if args.mix:
+    converter.mix_nozzles()
+
+if args.print:
+    print("----------------")
+    for comp in converter.components:
+        strout = "%s,\t, row: %f,\t corr %s,\t rot %f \t %s \t Nzl %s" % (comp.Designator, float(comp.RawRotation),
+                                                                          comp.Correction, float(comp.Rotation),
+                                                                          comp.Footprint, comp.nozzle)
+        print(strout)
+    print("----------------")
+
 converter.create_output_file(layer_flag)
